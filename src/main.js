9dc4caa35e51642bcb9f673a48e3502ac1b0dba2
Raphael <raphael@alsacreations.fr>
2025-05-31 15:26:44 +0200

    feat: mise à jour des configurations et des styles du projet

--- src/main.js
+++
@@ -1,1 +0,0 @@
-import "./css/styles.css";
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+// https://vitejs.dev/guide/build.html
+
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  // config options
+  plugins: [],
+})
