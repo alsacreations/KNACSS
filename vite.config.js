35d71252f4c19f163c1ba9bd897fc1fb1fecbb59
Raphael <raphael@alsacreations.fr>
2025-05-31 21:42:45 +0200

    fix: corrige les chemins d'importation et supprime les fichiers obsolètes

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,25 @@
 // https://vitejs.dev/guide/build.html
 
 import { defineConfig } from "vite"
+import { resolve } from "path"
 
-export default defineConfig({
-  // config options
-  base: "/KNACSS/",
-  plugins: [],
+// Correction pour __dirname en ES Module
+const __dirname = new URL(".", import.meta.url).pathname
+
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/KNACSS/" : "/"
+  return {
+    // config options
+    base: base, // Défini dynamiquement
+    appType: "mpa",
+    plugins: [],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          button: resolve(__dirname, "components/button/button.html"),
+        },
+      },
+    },
+  }
 })
