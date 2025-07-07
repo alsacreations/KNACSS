413276891ff311a3cf7dde47cba256ea5b662dfc
Raphael <raphael@alsacreations.fr>
2025-07-07 09:46:46 +0200

    refactor(postcss): migration de la configuration vers postcss.config.mjs et suppression de postcss.config.cjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
-
+import js from "@eslint/js"
+import globals from "globals"
+import pluginVue from "eslint-plugin-vue"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginVue.configs["flat/essential"],
-]);
+])
